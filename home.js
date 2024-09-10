e44dcc0f3423a0fb26be03cc94e1b354d6ed6fea
PaletiNeeraj <paletineeraj@gmail.com>
2024-09-10 10:16:59 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+let valueDisplys = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDisplys.forEach((valueDisply) => {
+    let startValue = 0;
+    let endValuel = parseInt(valueDisply.getAttribute("data-val"));
+    console.log(endValuel);
+    let duration = Math.floor(interval / endValuel);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisply.textContent = startValue;
+        if (startValue == endValuel) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
